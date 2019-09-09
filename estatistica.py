eae7f941f24b55b1009a6b4982f08d128d861652
felipe bastos <felipebastosweb@gmail.com>
2019-09-09 20:51:34 -0300

    Create estatistica.py

---
+++ estatistica.py
@@ -0,0 +1,10 @@
+from statistics import *
+
+media = mean
+mediana = median
+mediana_alta = median_high
+mediana_baixa = median_low
+desvio_padrao_populacional = pstdev
+variancia_populacional = pvariance
+desvio_padrao = stdev
+variancia = variance
