48f8eed14d9762fc9e4066c91e6ce503927e4475
felipe bastos <felipebastosweb@gmail.com>
2019-09-09 20:51:57 -0300

    Create matematica.py

---
+++ matematica.py
@@ -0,0 +1,24 @@
+from math import *
+
+arredondar = ceil
+arredondar_menor = floor
+truncar = trunc
+absoluto = fabs
+
+fatorial = factorial
+exponencial = exp
+logaritmo = log
+potenciacao = pow
+raiz_quadrada = sqrt
+
+seno = sin
+coseno = cos
+tangente = tan
+hipotenusa = hypot
+graus = degrees
+radianos = radians
+
+arco_seno = asin
+arco_coseno = acos
+arco_tangente = atang
+
